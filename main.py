9b388eea8b79c87860f054525e03ab9f159e64b9
Noah Selis <80969784+buckdooban@users.noreply.github.com>
2026-03-07 00:45:42 -0700

    here

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import argparse
+
+
 def main():
-    print("pomo")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("start", help="start the timer")
+    args = parser.parse_args()
+    print(args.start)
 
 
 if __name__ == "__main__":
