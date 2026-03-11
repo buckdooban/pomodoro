e4e3d0727e810b797fce744d5f6fda1b9571ecd6
Noah Selis <80969784+buckdooban@users.noreply.github.com>
2026-03-11 16:44:46 -0600

    just a big fat mess

---
+++ timer.py
@@ -0,0 +1,11 @@
+import time
+
+# prod constants
+# POMODORO_CYCLE = 1500  # 25 minutes
+# SHORT_BREAK = 300  # 5 minutes
+# LONG_BREAK = 900  # 15 minutes
+
+
+###
+# https://www.geeksforgeeks.org/python/how-to-create-a-countdown-timer-using-python/
+###
